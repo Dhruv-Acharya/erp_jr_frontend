bc36a3b330388ba5234a1c3d6d07aba9ed2985ec
Deepak Lalwani <deepak.lalwani81@gmail.com>
2018-11-13 17:41:27 +0530

    added confirm password field

--- src/components/CreateCompany.vue
+++ src/components/CreateCompany.vue
@@ -8,27 +8,43 @@
         <b-form-input v-model="text1"
                   type="text"
                   placeholder="Enter username" class="input"></b-form-input>
-        <b-form-input v-model="text1"
+        <b-form-input v-model="password"
                   type="password"
-                  placeholder="Enter Password" class="input"></b-form-input>
-
+                  placeholder="Confirm Password" class="input" @input="checkPassword()"></b-form-input>
+        <b-form-input v-model="reEnterPassword"
+                  type="password"
+                  placeholder="Enter Password" class="input" @input="checkPassword()"></b-form-input>
+        <span style="color:red" v-if="this.mismatch">Passwords does not match</span>
+        <div>
         <b-button size="md" variant="primary">
                 Create
             </b-button>
+        </div>
     </div>
 </template>
 <script>
 export default {
   data () {
     return {
-
+      mismatch: false,
+      password: '',
+      reEnterPassword: ''
+    }
+  },
+  methods: {
+    checkPassword () {
+      if (this.password !== this.reEnterPassword) {
+        this.mismatch = true
+      } else {
+        this.mismatch = false
+      }
     }
   }
 }
 </script>
 <style>
-.input{
-  width:40%;
-  margin:10px auto
+.input {
+  width: 40%;
+  margin: 10px auto;
 }
 </style>
