c3975218c70d8eb2d1804523ad08b759fcb56145
Deepak Lalwani <deepak.lalwani81@gmail.com>
2018-11-13 15:23:46 +0530

    added login and create company page

---
+++ src/components/CreateCompany.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1> Jalaram rakhi</h1>
+
+        <b-form-input v-model="text1"
+                  type="text"
+                  placeholder="Enter Company name" class="input"></b-form-input>
+        <b-form-input v-model="text1"
+                  type="text"
+                  placeholder="Enter username" class="input"></b-form-input>
+        <b-form-input v-model="text1"
+                  type="password"
+                  placeholder="Enter Password" class="input"></b-form-input>
+
+        <b-button size="md" variant="primary">
+                Create
+            </b-button>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style>
+.input{
+  width:40%;
+  margin:10px auto
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1> Jalaram rakhi</h1>
+
+        <b-form-select v-model="selected" :options="options" class="mb-3 input" placeholder="Select Company" />
+
+        <b-form-input v-model="text1"
+                  type="text"
+                  placeholder="Enter username" class="input"></b-form-input>
+        <b-form-input v-model="text1"
+                  type="password"
+                  placeholder="Enter Password" class="input"></b-form-input>
+
+        <b-button size="md" variant="primary">
+                Login
+            </b-button>
+        <div>
+        <b-button size="md" variant="link" @click="toCreate()">
+                Create new company
+            </b-button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: 'Please select an option' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' }
+      ]
+    }
+  },
+  methods: {
+    toCreate () {
+      this.$router.push({name: 'CreateCompany'})
+    }
+  }
+}
+</script>
+<style>
+.input{
+  width:40%;
+  margin:10px auto
+}
+</style>
