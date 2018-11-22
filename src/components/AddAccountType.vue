67142388ab2b32d2ce20011e720706b0cf9278f8
Adityaperiwal <adityaperiwal0007@gmail.com>
2018-11-22 14:06:09 +0530

    updated vue files

--- src/components/AddAccountType.vue
+++ src/components/AddAccountType.vue
@@ -4,15 +4,30 @@
         <h3> Add Account Type</h3>
         <!--Enter name of the unit-->
         <b-input-group  class="input">
-                <b-form-input id="account_type" placeholder="Enter Account Type"></b-form-input>
+                <b-form-input id="account_type" v-model="accountType" placeholder="Enter Account Type"></b-form-input>
         </b-input-group>
         <div>
-        <b-button id="addAccount" size="md" variant="primary">
+        <b-button id="addAccount" @click="addAccountType()" size="md" variant="primary">
                 Add
             </b-button>
         </div>
     </div>
 </template>
+<script>
+import userService from '../services/userService'
+export default {
+  data () {
+    return {
+      accountType: null
+    }
+  },
+  methods: {
+    async addAccountType () {
+      console.log(await userService.addAccountType(this.accountType))
+    }
+  }
+}
+</script>
 <style>
 .input {
   width: 40%;
